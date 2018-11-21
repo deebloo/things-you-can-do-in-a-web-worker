36cb6053fbca653168a6576d6248decb76405679
deebloo <dannybluedesign@gmail.com>
2018-11-21 17:44:08 -0500

    testing

---
+++ examples/proxy/proxy.worker.js
@@ -0,0 +1,11 @@
+importScripts('https://momentjs.com/downloads/moment-with-locales.min.js');
+
+self.onmessage = (msg) => {
+    console.log(msg);
+    
+    switch(msg.data.type) {
+        case 'FORMAT':
+            self.postMessage(moment(msg.data.date).format());
+            break;
+    }
+}
